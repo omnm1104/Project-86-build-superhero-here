ddbb342007fb20a1e26a4c8d0ad70df599c2a537
omnm1104 <82947324+omnm1104@users.noreply.github.com>
2021-05-04 17:27:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = new fabric.Canvas("myCanvas");
+
+var canvas = new fabric.Canvas("myCanvas");
+
+player_x = 10;
+player_y = 10;
+
+block_width = 30;
+block_height = 30;
+
+player_object = "";
+
+block_object = "";
